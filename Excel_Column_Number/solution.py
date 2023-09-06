7cda8b08565fc8e84a4fdb4548ff336360d9c5d0
Sarah Cohen <cohen.sarah591@gmail.com>
2023-09-06 12:41:34 -0400

    Update solution.py

--- Excel_Column_Number/solution.py
+++ Excel_Column_Number/solution.py
@@ -1,1 +1,12 @@
-
+def excel_to_column_number(N):
+  output = ""
+  while N>0:
+    # // find the remainder of the number % 26 (the number of characters)
+    remainder = N % 26
+    letter = chr(remainder + 97)
+    # // put that character in the next place
+    output = letter + output
+    # // floor = how many times 26 will fit into number
+    N = N//26
+  
+  return output
