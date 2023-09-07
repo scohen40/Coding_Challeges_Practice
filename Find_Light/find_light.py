465e762dd655481027980d9860170a49c6c8e9fd
Sarah Cohen <cohen.sarah591@gmail.com>
2023-09-07 14:23:20 -0400

    Create find_light.py

---
+++ Find_Light/find_light.py
@@ -0,0 +1,42 @@
+## You are a software engineer at a computer vision startup
+## your job is to find the location of a 2x2 light within a black image
+## you are given the image as a 2D array of 0s and 1s:
+image2 = [ 
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+] #returns [9,6]
+
+image = [ 
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+] ## return: [8,5]
+
+## write a function that takes in an image like the one above
+## the image will be of arbitrary size
+## return the X,Y of the bottom-right corner
+
+#FIRST ATTEMPT
+def find_light_1(image):
+  # search each row from the bottom right for a 1
+  for row in range(len(image)-1, 0, -1):
+    for col in range(len(image[row])-1, 0, -1):
+      if image[row][col] == 1:
+        return [col, row]
+
+print(find_light_1(image2))
