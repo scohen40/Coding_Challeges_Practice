5e0037b5b1d93eb76258d583047ad3a83c432176
Sarah Cohen <cohen.sarah591@gmail.com>
2023-09-22 18:38:32 +0000

    wrote code for log pieces extraction, map adding unfinished

--- Analyze_Logs/analyze_logs.py
+++ Analyze_Logs/analyze_logs.py
@@ -61,3 +61,43 @@ output["WARNING"]["403"]["Forbidden"]["No token in request parameters"] # 3
 
 # When printed it will more likely look like this:
 printed_output = {'WARNING': {'403': {'Forbidden': {'No token in request parameters': 3}}}, 'ERROR': {'500': {'Server Error': {'int is not subscriptable': 8}}}, 'INFO': {'200': {'OK': {'Login Successful': 6, 'User sent a message': 6}}}}
+
+#assuptions
+#- each log is ended by a newline character
+def analyze_logs(logsString):
+    logsMap = {}
+    
+    #split the string into individual logs by splitting by line breaks
+    logs = logsString.split('\n')
+    # print(logs)
+    #loop through each log, find the log type, then the log code, then the log title, and finally the log message
+    for log in logs[1:len(logs)-1]:
+        
+        #split each log into individual tokens/words by the spaces
+        log = log.split()
+        #get the log type
+        logType = log[0].strip('[]')
+        #get the log code
+        logCode = log[1]
+        #get the log title
+        logTitle = ''
+        #keep adding to the title if there's no ':'
+        currentIndex = 2
+        for word in log[2:]:
+            currentIndex += 1 #increment for the next loop
+            #add the word to the logTitle
+            if ':' in word:
+                logTitle += word.strip(':')
+                break
+            else:
+                logTitle += word + ' '
+        #get the log message
+        logMessage = ' '.join(log[currentIndex:])
+
+        #find/add all of that to the logsMap and add 1 to the count at that spot
+        ## TO DO : SPLIT THE FOLLOWING STATEMENT UP TO ADD EACH PIECE TO THE MAP INDIVIDUALLY, DOING IT ALL TOGETHER DOESN'T WORK
+        logsMap[logType][logCode][logTitle][logMessage] = logsMap.get(logType, logCode).get(logCode, logTitle).get(logTitle, logMessage).get(logMessage, 0) + 1
+
+    return logsMap
+
+print(analyze_logs(raw_logs))
